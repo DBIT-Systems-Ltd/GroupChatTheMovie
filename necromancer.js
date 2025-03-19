8575ab0edbf7c72eb00e46f8e2bcd1db6442994a
ShadeBlack <kyle@dbitsystemsltd.co.uk>
2025-03-19 10:14:05 +0000

    Add Instagram icon and update footer across multiple HTML files

--- necromancer.js
+++ necromancer.js
@@ -1,17 +1,15 @@
 // wait for the document to load
 document.addEventListener("DOMContentLoaded", function () {
-	// get the current year
-	const currentYear = new Date().getFullYear();
-
 	// add the current year to the footer span tag
+	const currentYear = new Date().getFullYear();
 	const footer = document.getElementById("footer");
 	const span = footer.getElementsByTagName("span")[0];
 	span.innerHTML = currentYear;
 
 	// apply bg-green-900 and animate-pulse classes to the link with the current page
 	let path = window.location.pathname;
+	console.log(path);
 	path = path.replace(".html", "");
-	if (path === "/") path = "/index";
 	const links = document.querySelectorAll("a");
 	links.forEach((link) => {
 		if (link.pathname === path) link.classList.add("bg-green-900", "animate-pulse");
